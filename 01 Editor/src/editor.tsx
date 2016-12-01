9d9f18fab84a99597f7120b73a1659b072577247
Braulio <braulio.diez@gmail.com>
2016-12-01 12:58:51 +0100

    Editor basic steps

---
+++ 01 Editor/src/editor.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+declare function require(name:string);
+const MTRC = require('markdown-to-react-components');
+
+interface Props {
+}
+
+interface State {
+  content : string;
+}
+
+export class EditorComponent extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {content: 'test'};
+  }
+
+
+  onTextareaChange(event) {
+      this.setState({
+        content: MTRC(event.target.value).tree
+      });
+    }
+
+  render() {
+    return (
+      <div>
+           <div>{this.state.content}</div>
+           <textarea onChange={this.onTextareaChange.bind(this)}/>
+      </div>
+    )
+  }
+}
