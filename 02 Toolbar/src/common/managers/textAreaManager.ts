a60a2d9f8d21e7460f9bf8a7e7c3ad83673f5dce
nasdan <daniel.sanchez@lemoncode.net>
2016-12-02 17:32:44 +0100

    first refactor textAreaManager

--- 02 Toolbar/src/common/managers/textAreaManager.ts
+++ 02 Toolbar/src/common/managers/textAreaManager.ts
@@ -1,34 +1,34 @@
 
 // Based on: http://stackoverflow.com/questions/1064089/inserting-a-text-where-cursor-is-using-javascript-jquery
-class TextAreaUtils {
-  insertAtCaret(txtArea: HTMLElement, text: string, offsetCursor : number = 0) {
-    const scrollPos : number = txtArea.scrollTop;
+class TextAreaManager {
+  insertAtCaret(textArea: HTMLTextAreaElement, text: string, offsetCursor : number = 0) {
+    const scrollPosition : number = textArea.scrollTop;
 
-    let position = this.calculateTextAreaCurrentCursorPosition(txtArea);
-    position = this.insertText(txtArea, text, position);
-    this.placeCursor(txtArea, position, offsetCursor);
+    let position = this.calculateTextAreaCurrentCursorPosition(textArea);
+    position = this.insertText(textArea, text, position);
+    this.placeCursor(textArea, position, offsetCursor);
 
-    txtArea.scrollTop = scrollPos;
+    textArea.scrollTop = scrollPosition;
   }
 
-  private calculateTextAreaCurrentCursorPosition(txtarea : HTMLElement)
+  private calculateTextAreaCurrentCursorPosition(txtarea : HTMLTextAreaElement)
   {
-      return txtarea['selectionStart'];
+      return txtarea.selectionStart;
   }
 
-  private insertText(txtarea : HTMLElement, text : string, position : number) : number {
-    var front = (txtarea['value']).substring(0, position);
-    var back = (txtarea['value']).substring(position, txtarea['value'].length);
-    txtarea['value'] = front + text + back;
+  private insertText(textArea : HTMLTextAreaElement, text : string, position : number) : number {
+    var preText = (textArea.value).substring(0, position);
+    var postText = (textArea.value).substring(position, textArea.value.length);
+    textArea.value = preText + text + postText;
     return (position + text.length);
   }
 
-  private placeCursor(txtArea : HTMLElement, position : number, offset : number)
+  private placeCursor(txtArea : HTMLTextAreaElement, position : number, offset : number)
   {
-    txtArea['selectionStart'] = position - offset;
-    txtArea['selectionEnd'] = position - offset;
+    txtArea.selectionStart = position - offset;
+    txtArea.selectionEnd = position - offset;
     txtArea.focus();
   }
 }
 
-const textAreaUtils = new TextAreaUtils();
+export const textAreaManager = new TextAreaManager();
--- 02 Toolbar/src/editor.tsx
+++ 02 Toolbar/src/editor.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 declare function require(name:string);
 const MTRC = require('markdown-to-react-components');
+import {textAreaManager} from './common/managers/textAreaManager';
 
 interface Props {
 }
@@ -10,8 +11,7 @@ interface State {
 }
 
 export class EditorComponent extends React.Component<Props, State> {
-
-  textarea : any;
+  textArea : any;
 
   constructor(props: Props) {
     super(props);
@@ -19,61 +19,10 @@ export class EditorComponent extends React.Component<Props, State> {
     this.state = {content: 'test'};
   }
 
-  //http://stackoverflow.com/questions/1064089/inserting-a-text-where-cursor-is-using-javascript-jquery
-  //https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByClassName
   //https://facebook.github.io/react/docs/refs-and-the-dom.html
-  // offset, pending IE fix
-  // TODO: Refactor, clean code this
-  // Proposal
-  //
-  // insertAtCaret(txtArea: HMLElement, text: string, offsetCursor = 0) {
-  //  const scrollPos : number = txtarea.scrollTop;
-  //  let strPos : number = 0;
-  //
-  //  strPos = calculateTextAreaCurrentCursorPosition(txtarea);
-  //  insertText(txtArea, strPos, text);
-  //  placeCursor(txtArea, strPos, offset);
-  // }
-  insertAtCaret(txtarea : HTMLElement, text, offsetCursor = 0) {
-  		var scrollPos = txtarea.scrollTop;
-  		var strPos = 0;
-      // TODO: this "ie" flag seems to be for old Internet explorer versions
-      // if it's above 11 it fallbacks into "ff" we can simplify this
-  		var br = ((txtarea['selectionStart'] || txtarea['selectionStart'] == '0') ?
-  			"ff" : (document['selection'] ? "ie" : false ) );
-  		if (br == "ie") {
-  			txtarea.focus();
-  			var range = document['selection'].createRange();
-  			range.moveStart ('character', -txtarea['value'].length);
-  			strPos = range.text.length;
-  		} else if (br == "ff") {
-  			strPos = txtarea['selectionStart'];
-  		}
-
-  		var front = (txtarea['value']).substring(0, strPos);
-  		var back = (txtarea['value']).substring(strPos, txtarea['value'].length);
-  		txtarea['value'] = front + text + back;
-  		strPos = strPos + text.length;
-  		if (br == "ie") {
-        // TODO: This is old IE consider removing this code
-  			txtarea.focus();
-  			var ieRange = document['selection'].createRange();
-  			ieRange.moveStart ('character', -txtarea['value'].length);
-  			ieRange.moveStart ('character', strPos);
-  			ieRange.moveEnd ('character', 0);
-  			ieRange.select();
-  		} else if (br == "ff") {
-  			txtarea['selectionStart'] = strPos - offsetCursor;
-  			txtarea['selectionEnd'] = strPos - offsetCursor;
-  			txtarea.focus();
-  		}
-
-  		txtarea.scrollTop = scrollPos;
-  	}
-
-  onBoldText(event) {
+  onItalicText(event) {
     event.preventDefault();
-    this.insertAtCaret(this.textarea, '**', 1)
+    textAreaManager.insertAtCaret(this.textArea, '**', 1);
   }
 
   onTextareaChange(event) {
@@ -85,7 +34,7 @@ export class EditorComponent extends React.Component<Props, State> {
   render() {
     return (
       <div>
-        <input type="submit" value="Italic" className="btn btn-default" onClick={this.onBoldText.bind(this)} />
+        <input type="submit" value="Italic" className="btn btn-default" onClick={this.onItalicText.bind(this)} />
         <div className='editor--container-flex'>
          <div className='editor--viewer-border editor--viewer-container'>
            {this.state.content}
@@ -95,7 +44,7 @@ export class EditorComponent extends React.Component<Props, State> {
              id="editor-viewer-text-area"
              className='editor--textarea-size'
              onChange={this.onTextareaChange.bind(this)}
-             ref={(textarea) => { this.textarea = textarea; }}
+             ref={(textarea) => { this.textArea = textarea; }}
              >
              {this.state.content}
            </textarea>
