b5e5f93c44b9f7c6ca2117684f87fb7b62e0cf56
nasdan <daniel.sanchez@lemoncode.net>
2016-12-14 18:19:53 +0100

    Add quote button

--- 02 Toolbar/src/components/toolbar/buttons/index.tsx
+++ 02 Toolbar/src/components/toolbar/buttons/index.tsx
@@ -2,10 +2,12 @@ import {ToolbarButton} from './toolbarButton';
 import {ItalicButton} from './italicButton';
 import {BoldButton} from './boldButton';
 import {HeaderButton} from './headerButton';
+import {QuoteButton} from './quoteButton'
 
 export {
   ToolbarButton,
   ItalicButton,
   BoldButton,
-  HeaderButton
+  HeaderButton,
+  QuoteButton
 }
--- 02 Toolbar/src/components/toolbar/index.tsx
+++ 02 Toolbar/src/components/toolbar/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {
-  ToolbarButton, ItalicButton, BoldButton, HeaderButton
+  ToolbarButton, ItalicButton, BoldButton, HeaderButton,
+  QuoteButton
 } from './buttons';
 
 interface Props {
@@ -30,6 +31,29 @@ export const Toolbar = (props: Props) => {
         </ToolbarButton>
 
         <ToolbarButton textArea={props.textArea} caret="**" offset={1}
+          updateTextArea={props.updateTextArea}
+          shouldUpdateCursor={props.shouldUpdateCursor}
+          cursorPosition={props.cursorPosition}>
+          <ItalicButton />
+        </ToolbarButton>
+      </div>
+
+      <div className="btn-group">
+        <ToolbarButton textArea={props.textArea} caret="> " offset={2}
+          updateTextArea={props.updateTextArea}
+          shouldUpdateCursor={props.shouldUpdateCursor}
+          cursorPosition={props.cursorPosition}>
+          <QuoteButton />
+        </ToolbarButton>
+
+        <ToolbarButton textArea={props.textArea} caret="``" offset={1}
+          updateTextArea={props.updateTextArea}
+          shouldUpdateCursor={props.shouldUpdateCursor}
+          cursorPosition={props.cursorPosition}>
+          <BoldButton />
+        </ToolbarButton>
+
+        <ToolbarButton textArea={props.textArea} caret="[](url)" offset={1}
           updateTextArea={props.updateTextArea}
           shouldUpdateCursor={props.shouldUpdateCursor}
           cursorPosition={props.cursorPosition}>
