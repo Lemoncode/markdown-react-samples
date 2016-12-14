b50ed192aa4ea95fedaa6600d2850d733dc73eff
nasdan <daniel.sanchez@lemoncode.net>
2016-12-14 12:11:06 +0100

    Remove unnecessary styles

--- 02 Toolbar/src/main.tsx
+++ 02 Toolbar/src/main.tsx
@@ -2,9 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { EditorComponent } from './editor'
 
-
-const input = '# This is a header example \nThis not!'
-
 ReactDOM.render(
   <EditorComponent>
   </EditorComponent>
